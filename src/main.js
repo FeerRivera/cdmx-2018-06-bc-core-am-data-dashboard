ee6308922e72b516fb348cfc9c63a1b0817795ae
unknown <feerkay15@gmail.com>
2018-06-22 17:20:02 -0500

    Usuario y contraseña, icono en el titulo

---
+++ src/main.js
@@ -0,0 +1,17 @@
+function Login(){
+var done=0;
+var usuario=document.login.usuario.value;
+var password=document.login.password.value;
+if (usuario=="USUARIO1" && password=="CONTRASEÑA1") {
+window.location="file:///C:/Users/TurnoAM/Documents/proyecto2/cdmx-2018-06-bc-core-am-data-dashboard/src/index2.html";
+}
+if (usuario=="USUARIO2" && password=="CONTRASEÑA2") {
+window.location="file:///C:/Users/TurnoAM/Documents/proyecto2/cdmx-2018-06-bc-core-am-data-dashboard/src/index2.html";
+}
+if (usuario=="" && password=="") {
+window.location="file:///C:/Users/TurnoAM/Documents/proyecto2/cdmx-2018-06-bc-core-am-data-dashboard/src/index2.html";
+}
+}
+
+
+document.oncontextmenu = function(){return false}
