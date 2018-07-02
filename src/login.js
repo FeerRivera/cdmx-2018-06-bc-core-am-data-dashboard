1ce8d52501523ed33f3e69b83c97dff2c26bb2b1
FeerRivera <39814132+FeerRivera@users.noreply.github.com>
2018-07-02 12:04:49 -0500

    Update login.js
    ya funciona

--- src/login.js
+++ src/login.js
@@ -1,12 +1,14 @@
-loginButton.addEventListener("click", function(){
-    login();
-})
+let data = document.forms['data']
+button = document.getElementById('button')
+button.addEventListener('click', validate)
 
-let login = () => {
-    let use = userName.value;
-    let contr = contraseña.value;
-
-    if(use === "lucile@laboratoria.la"  && contraseña === "123"){
-      return window.location = "file:///C:/Users/rodri/Downloads/cdmx-2018-06-bc-core-am-data-dashboard-master/cdmx-2018-06-bc-core-am-data-dashboard-master/src/datadash.html"
-
+function validate(e) {
+    e.preventDefault();
+    let user = data.querySelector('input[type="text"]')
+    let password = data.querySelector('input[type="password"]')
+    if (user.value === 'lucile@laboratoria.la' && password.value === '123') {
+        window.location.href = 'index.html'
+    } else {
+        alert('Ingresa una cuenta válida')
+    }
 }
