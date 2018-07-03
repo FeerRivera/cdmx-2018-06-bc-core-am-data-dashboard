7d513e78ab70b2b3c332ec9567fbce77c7b4cc8a
FeerRivera <39814132+FeerRivera@users.noreply.github.com>
2018-07-02 20:02:26 -0500

    Update login.js
    funcion inicio

--- src/login.js
+++ src/login.js
@@ -1,14 +1,14 @@
-let data = document.forms['data']
-button = document.getElementById('button')
-button.addEventListener('click', validate)
+let data = document.forms["data"]
+button = document.getElementById("button")
+button.addEventListener("click", cuenta)
 
-function validate(e) {
-    e.preventDefault();
+function cuenta(log) {
+    log.preventDefault();
     let user = data.querySelector('input[type="text"]')
     let password = data.querySelector('input[type="password"]')
-    if (user.value === 'lucile@laboratoria.la' && password.value === '123') {
-        window.location.href = 'index.html'
+    if (user.value === "lucile@laboratoria.la" && password.value === "123") {
+        window.location.href = "datadash.html"
     } else {
-        alert('Ingresa una cuenta válida')
+        alert("Ingresa una cuenta válida")
     }
 }
