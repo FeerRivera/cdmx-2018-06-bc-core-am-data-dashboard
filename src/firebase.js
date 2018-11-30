29f121f9f838911d582664ff16e16508689daad1
FeerRivera <feerkay15@gmail.com>
2018-11-29 22:52:05 -0600

    agregando firebase al login

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDDCq_D3f21_FGwwC-HOncbRAvesgeZBwA",
+    authDomain: "datadash-7af25.firebaseapp.com",
+    databaseURL: "https://datadash-7af25.firebaseio.com",
+    projectId: "datadash-7af25",
+    storageBucket: "datadash-7af25.appspot.com",
+    messagingSenderId: "791088192388"
+  };
+  firebase.initializeApp(config);
--- src/login.js
+++ src/login.js
@@ -1,14 +1,38 @@
-let data = document.forms["data"]
-button = document.getElementById("button")
-button.addEventListener("click", cuenta)
+// let data = document.forms["data"]
+// button = document.getElementById("button")
+// button.addEventListener("click", cuenta)
 
-function cuenta(log) {
-    log.preventDefault();
-    let user = data.querySelector('input[type="text"]')
-    let password = data.querySelector('input[type="password"]')
-    if (user.value === "lucile@laboratoria.la" && password.value === "123") {
-        window.location.href = "index.html"
-    } else {
-        alert("Ingresa una cuenta válida usuario: lucile@laboratoria.la contraseña:123")
-    }
-}
+// function cuenta(log) {
+//     log.preventDefault();
+//     let user = data.querySelector('input[type="text"]')
+//     let password = data.querySelector('input[type="password"]')
+//     if (user.value === "lucile@laboratoria.la" && password.value === "123") {
+//         window.location.href = "index.html"
+//     } else {
+//         alert("Ingresa una cuenta válida usuario: lucile@laboratoria.la contraseña:123")
+//     }
+// }
+
+const btnentrar = document.getElementById('entrar');
+
+
+const entrar = () => {
+  const email1 = document.getElementById('email1').value;
+  const password1 = document.getElementById('password1').value;
+  localStorage.setItem('email1', email1);
+  firebase.auth().signInWithEmailAndPassword(email1, password1)
+    .then(function() {
+      // promise.catch(console.log(e.message));
+      window.location.href = 'index.html';
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      alert('Verifica tus datos');
+      // ...
+    });
+};
+btnentrar.addEventListener('click', entrar);
